04a8716a55bc6c9291f5d6a49a6bcce39fd7fd3a
Liiked <linyuanqianpp@126.com>
2017-04-05 17:11:04 +0800

    使用require加载业务模块

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,25 @@
 require.config({
-    // baseUrl: '../js',
+    baseUrl: '../js',
     paths: {
         "jquery": 'https://cdn.bootcss.com/jquery/3.1.1/jquery.min',
         "Vue": 'https://cdn.bootcss.com/vue/2.2.4/vue',
-        'polyfill': 'https://cdn.bootcss.com/babel-polyfill/7.0.0-alpha.1/polyfill.js',
-        'Vue_router': 'https://cdn.bootcss.com/vue-router/2.3.0/vue-router.js',
-    }
+        'polyfill': 'https://cdn.bootcss.com/babel-polyfill/7.0.0-alpha.1/polyfill',
+        'VueRouter': ['https://cdn.bootcss.com/vue-router/2.3.0/vue-router', 'vendor/vue-router'],
+        'router': 'router',
+    },
+    shim: {
+        'VueRouter': {
+            deps: ['Vue'],
+            exports: 'VueRouter'
+        }
+    },
+
+})
+
+require.onError = function(msg) {
+    console.warn(msg)
+}
+
+require(['router'], function(router) {
+    router.setTitle(location.pathname)
 })
--- app/js/router.js
+++ app/js/router.js
@@ -1,16 +1,28 @@
 define(function() {
     var route = {
         '/require': 'AMD异步模块定义',
-        '/layout': '测试合成gulp-file'
+        '/layout': '测试合成gulp-file',
+        '/router': '路由'
     }
 
-    function setTitle() {
-        var pathname = /\/\w+\./.exec(location.pathname)[0].split('.')[0]
-        console.log(pathname)
-        if (!route[pathname]) {
-            return
+    var path = '',
+        pathname = '';
+
+    return {
+        setTitle: function(path) {
+            pathname = /\/\w+\./.exec(path)[0].split('.')[0]
+            console.log(pathname)
+            if (!route[pathname]) {
+                return
+            }
+            document.title = route[pathname]
+            try {
+                require(['business' + pathname])
+            } catch (error) {
+                console.log(error)
+            }
+
         }
-        document.title = route[pathname]
     }
-    setTitle();
+
 });
