9c3e89d5dc7cd21215a74461ba6d53ece9df9aeb
Liiked <linyuanqianpp@126.com>
2017-04-05 14:44:44 +0800

    业务整理到main和router文件中去

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@ require.config({
     paths: {
         "jquery": 'https://cdn.bootcss.com/jquery/3.1.1/jquery.min',
         "Vue": 'https://cdn.bootcss.com/vue/2.2.4/vue',
-        "sync": "sync_a"
+        'polyfill': 'https://cdn.bootcss.com/babel-polyfill/7.0.0-alpha.1/polyfill.js',
+        'Vue_router': 'https://cdn.bootcss.com/vue-router/2.3.0/vue-router.js',
     }
 })
